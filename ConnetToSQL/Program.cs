4131a8652c3a9e2668cb98804be900eced921bb8
MrUmmataliyev <mr.umataliev.1817@gmail.com>
2024-02-08 10:35:35 +0500

    Buds fixed

--- ConnetToSQL/Program.cs
+++ ConnetToSQL/Program.cs
@@ -9,7 +9,8 @@ class Program
         using NpgsqlConnection connection = new NpgsqlConnection(connectionString);
         Program.Create(connection);
         Program.Read(connection);
-
+        Program.Update(connection);
+        Program.Delete(connection);
     }
     static void Create(NpgsqlConnection connection)
     {
@@ -31,6 +32,7 @@ class Program
         connection.Close();
     }
 
+
     static void Read(NpgsqlConnection connection)
     {
         connection.Open();
@@ -44,4 +46,48 @@ class Program
         }
         connection.Close();
     }
+
+
+
+    static void Update(NpgsqlConnection connection)
+    {
+        connection.Open();
+        Console.Write("Which id do you want to update: ");
+        int id = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter new user_name: ");
+        string name = Console.ReadLine();
+        Console.Write("Enter new user_surname: ");
+        string surname = Console.ReadLine();
+        Console.Write("Enter new age: ");
+        int age = int.Parse(Console.ReadLine());
+
+        string query = $"UPDATE users SET user_name = '{name}', user_surname = '{surname}', age = '{age}' WHERE user_id = {id};";
+        using NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
+
+        cmd.ExecuteNonQuery();
+
+        Console.WriteLine($"Successfully updated");
+
+        connection.Close();
+    }
+
+    static void Delete(NpgsqlConnection connection)
+    {
+        connection.Open();
+        Console.Write("Which id do you want to delete: ");
+        int id = int.Parse(Console.ReadLine());
+
+
+
+        string query = $"DELETE from users where id = {id};";
+        using NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
+
+        cmd.ExecuteNonQuery();
+
+        Console.WriteLine($"Successfully deleted");
+
+        connection.Close();
+    }
+
 }
