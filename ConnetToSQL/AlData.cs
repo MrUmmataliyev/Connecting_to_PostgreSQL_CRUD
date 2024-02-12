8a07a0067f71fa25aa4133e3d2a754c03f7e8aa7
MrUmmataliyev <mr.umataliev.1817@gmail.com>
2024-02-12 08:47:25 +0500

    Update +

--- ConnetToSQL/AlData.cs
+++ ConnetToSQL/AlData.cs
@@ -1,12 +1,85 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Npgsql;
+
 
 namespace ConnetToSQL
 {
-    internal class AlData
+    public class AlData
     {
+        static void Create(NpgsqlConnection connection)
+        {
+            connection.Open();
+            Console.Write("Enter user_name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter user_surname: ");
+            string surname = Console.ReadLine();
+            Console.Write("Enter age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            string query = $"insert into users (user_name, user_surname, age) values ('{name}','{surname}',{age});";
+            using NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
+
+            cmd.ExecuteNonQuery();
+
+            Console.WriteLine($"Successfully created");
+
+            connection.Close();
+        }
+
+
+        static void Read(NpgsqlConnection connection)
+        {
+            connection.Open();
+            string query = "select * from users";
+            using NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
+
+            var result = cmd.ExecuteReader();
+            while (result.Read())
+            {
+                Console.WriteLine($"{result[0]}|{result[1]}|{result[2]}|{result[3]}");
+            }
+            connection.Close();
+        }
+
+
+        static void Update(NpgsqlConnection connection)
+        {
+            connection.Open();
+            Console.Write("Which id do you want to update: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter new user_name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter new user_surname: ");
+            string surname = Console.ReadLine();
+            Console.Write("Enter new age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            string query = $"UPDATE users SET user_name = '{name}', user_surname = '{surname}', age = '{age}' WHERE user_id = {id};";
+            using NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
+
+            cmd.ExecuteNonQuery();
+
+            Console.WriteLine($"Successfully updated");
+
+            connection.Close();
+        }
+
+        static void Delete(NpgsqlConnection connection)
+        {
+            connection.Open();
+            Console.Write("Which id do you want to delete: ");
+            int id = int.Parse(Console.ReadLine());
+
+
+
+            string query = $"DELETE from users where id = {id};";
+            using NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
+
+            cmd.ExecuteNonQuery();
+
+            Console.WriteLine($"Successfully deleted");
+
+            connection.Close();
+        }
     }
 }
--- ConnetToSQL/Program.cs
+++ ConnetToSQL/Program.cs
@@ -8,86 +8,10 @@ class Program
 
         using NpgsqlConnection connection = new NpgsqlConnection(connectionString);
         //Program.Create(connection);
-        Program.Read(connection);
+        //Program.Read(connection);
         //Program.Update(connection);
         //Program.Delete(connection);
     }
-    static void Create(NpgsqlConnection connection)
-    {
-        connection.Open();
-        Console.Write("Enter user_name: ");
-        string name = Console.ReadLine();
-        Console.Write("Enter user_surname: ");
-        string surname = Console.ReadLine();
-        Console.Write("Enter age: ");
-        int age =  int.Parse(Console.ReadLine());
-
-        string query = $"insert into users (user_name, user_surname, age) values ('{name}','{surname}',{age});";
-        using NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
-
-        cmd.ExecuteNonQuery();
-
-        Console.WriteLine($"Successfully created");
-
-        connection.Close();
-    }
-
-
-    static void Read(NpgsqlConnection connection)
-    {
-        connection.Open();
-        string query = "select * from users";
-        using NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
-
-        var result = cmd.ExecuteReader();
-        while (result.Read())
-        {
-            Console.WriteLine($"{result[0]}|{result[1]}|{result[2]}|{result[3]}");
-        }
-        connection.Close();
-    }
-    //do hometasks
-
-
-    static void Update(NpgsqlConnection connection)
-    {
-        connection.Open();
-        Console.Write("Which id do you want to update: ");
-        int id = int.Parse(Console.ReadLine());
-
-        Console.Write("Enter new user_name: ");
-        string name = Console.ReadLine();
-        Console.Write("Enter new user_surname: ");
-        string surname = Console.ReadLine();
-        Console.Write("Enter new age: ");
-        int age = int.Parse(Console.ReadLine());
-
-        string query = $"UPDATE users SET user_name = '{name}', user_surname = '{surname}', age = '{age}' WHERE user_id = {id};";
-        using NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
-
-        cmd.ExecuteNonQuery();
-
-        Console.WriteLine($"Successfully updated");
-
-        connection.Close();
-    }
-
-    static void Delete(NpgsqlConnection connection)
-    {
-        connection.Open();
-        Console.Write("Which id do you want to delete: ");
-        int id = int.Parse(Console.ReadLine());
-
-
-
-        string query = $"DELETE from users where id = {id};";
-        using NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
-
-        cmd.ExecuteNonQuery();
-
-        Console.WriteLine($"Successfully deleted");
-
-        connection.Close();
-    }
+   
 
 }
